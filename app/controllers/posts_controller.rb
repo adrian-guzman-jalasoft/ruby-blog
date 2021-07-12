42799e0803ed9efabff2a39dda20923fce9a7d41
Adrian Guzman <adrian.guzman@jalasoft.com>
2021-07-12 10:24:00 -0400

    configure posts controller and vews to create posts and show posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+    def index
+        @user = User.find(params[:user_id])
+        puts "before conditional"
+        @posts = if @user
+            puts "there are posts "
+            @user.posts
+        end
+        puts "after conditional"
+    end
+
+    def new
+       @user = User.find(params[:user_id])
+       @post = Post.new
+    end
+    
+    def create
+        @user = User.find(params[:user_id])
+        @post = @user.posts.create(post_params)
+        redirect_to user_path(@user)
+    end
+
+    private
+        def post_params
+            params.require(:post).permit(:title, :content)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,4 @@ Rails.application.routes.draw do
   resources :users do
     resources :posts
   end
-
-  #posts
 end
